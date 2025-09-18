00519a6faae6874b10650032f809d2a849f876de
Techno Tumbleweeds <hunterccreech@gmail.com>
2025-09-18 17:54:13 -0600

    Update Drivetrain.java
    Added TeleOp movement subsystem

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Hardware/Subsystems/Drivetrain.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Hardware/Subsystems/Drivetrain.java
@@ -5,7 +5,12 @@ import org.firstinspires.ftc.teamcode.Hardware.RobotHardware;
 public class Drivetrain {
     RobotHardware robot  = new RobotHardware();
 
-    public void drive(double LeftStickY, double LeftStickX, double RightStickY, double RightStickX){
+    public void drive(double LeftStickY, double LeftStickX, double RightStickY, double RightStickX, double motorSpeed){
+        
+        robot.rightFront.setPower((( - RightStickY - LeftStickX) - (RightStickX)) * motorSpeed);
+        robot.rightBack.setPower((( - RightStickY - LeftStickX) + (RightStickX)) * motorSpeed);
+        robot.leftBack.setPower((( - RightStickY + LeftStickX) - (RightStickX)) * motorSpeed);
+        robot.leftFront.setPower((( - RightStickY + LeftStickX) + (RightStickX)) * motorSpeed);
 
     }
 }
