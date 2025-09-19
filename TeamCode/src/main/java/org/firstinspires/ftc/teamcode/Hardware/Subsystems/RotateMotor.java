6da384a054b6297635c558ce3faa740f8bbc211d
Techno Tumbleweeds <hunterccreech@gmail.com>
2025-09-18 20:00:25 -0600

    Added Sorter v1 Code
    Added system for sorting

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Hardware/Subsystems/RotateMotor.java
@@ -0,0 +1,45 @@
+package org.firstinspires.ftc.teamcode.Hardware.Subsystems;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+import org.firstinspires.ftc.teamcode.Hardware.RobotHardware;
+
+import org.firstinspires.ftc.teamcode.Software.Variables;
+
+public class RotateMotor {
+    RobotHardware robot;
+
+    public RotateMotor(RobotHardware robot) {
+        this.robot = robot;
+    }
+
+        public void rotateSorter(int targetPos) {
+            int position = 5;
+            boolean motorPower = false;
+
+            if (targetPos == 0) {
+                motorPower = true;
+                position = Variables.intake1;
+            } else if (targetPos == 1) {
+                motorPower = true;
+                position = Variables.intake2;
+            } else if (targetPos == 2) {
+                motorPower = true;
+                position = Variables.intake3;
+            } else {
+                motorPower = false;
+                robot.testMotor.setPower(0);
+            }
+
+            if (motorPower) {
+                // Set target ONCE
+                robot.testMotor.setTargetPosition(position);
+                // Make sure mode is correct once at init, not every loop
+                robot.testMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+                // Keep power nonzero so PID loop can hold it
+                robot.testMotor.setPower(0.25);
+            }else if (!motorPower){
+                robot.testMotor.setPower(0);
+            }
+        }
+
+    }
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Software/Variables.java
@@ -0,0 +1,15 @@
+package org.firstinspires.ftc.teamcode.Software;
+
+public class Variables {
+    // Sorter Positions
+    public static final int intake1 = 0;
+    public static final int intake2 = -125;
+    public static final int intake3 = -250;
+
+    // Launch Positions
+    public static final int launch1 = 3;
+    public static final int launch2 = 4;
+    public static final int launch3 = 4;
+
+
+}
