b98f9419e842ed6d9b2f863a83d8d46b96f9ec30
Techno Tumbleweeds <technotumbleweeds@gmail.com>
2025-12-15 19:10:13 -0700

    Hood Control Works

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Hardware/Subsystems/HoodControl.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Hardware/Subsystems/HoodControl.java
@@ -10,12 +10,19 @@ public class HoodControl extends OpMode {
     RobotHardware rob;
     @Override
     public void init() {
+        rob = new RobotHardware();
         rob.init(hardwareMap);
     }
+
     @Override
     public void loop() {
-        //0.3826
-        rob.hood.setPosition();
+        double stick = -gamepad2.right_stick_y;
+
+        double targetPosition =
+                0.3826 + (1.0 - 0.3826) * ((stick + 1.0) / 2.0);
+
+        rob.hood.setPosition(targetPosition);
     }
+
 
 }
